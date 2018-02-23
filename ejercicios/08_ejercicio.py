ea18484fd9b696dd422b4555f15ddb491778c8f2
JoseGpeSoto <joseguadalupesotolopez@gmail.com>
2018-02-23 09:29:04 -0600

    OK

--- ejercicios/08_ejercicio.py
+++ ejercicios/08_ejercicio.py
@@ -20,7 +20,6 @@ Resultado: 1778400
 matrix = [[20,1,20,8,6],[19,0,5,8,10],[9,8,9,0,12],[11,2,13,4,5],[4,3,18,1,3]]
 arr =[]
 for i in matrix:
-	cont = 1
 	for j in i:
 		arr.append(j)
 arr = sorted(arr)
