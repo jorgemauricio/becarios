43793868886318a011083920f7ce7073d84ca952
Miguel-INIF <Miguel_ING_Amador@outlook.com>
2018-02-25 08:32:51 -0600

    Ejercicio 9, 12 y 13

--- ejercicios/09_ejercicio.py
+++ ejercicios/09_ejercicio.py
@@ -17,3 +17,16 @@ matrix = [[20,1,20,8,6],[19,0,5,8,10],[9,8,9,0,12],[11,2,13,4,5],[4,3,18,1,3]]
 Resultado: 1778400
 
 """
+matrix = [[20,1,20,8,6],[19,0,5,8,10],[9,8,9,0,12],[11,2,13,4,5],[4,3,18,1,3]]
+a, a2, resul = [], [], 1
+for i in range(0, len(matrix)):
+	for j in range(0, len(matrix[i])):
+		a.append(matrix[i][j])
+
+for i in range(0, 5):
+	maximo = max(a)
+	a2.append(maximo)
+	resul *= a2[i]
+	index = a.index(maximo)
+	a.pop(index)
+print("Resultado: {}".format(resul))
