0bffae49fea7d623631eb19d8fa4ee68fc43defc
jorgemauricio <jorge.ernesto.mauricio@gmail.com>
2018-03-05 22:39:02 -0600

    agregar examen 01

---
+++ ejercicios/01_examen.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+#######################################
+# Author: Jorge Mauricio
+# Email: jorge.ernesto.mauricio@gmail.com
+# Date: 2018-02-01
+# Version: 1.0
+# No. Examen 2
+# Valor: 10 puntos
+#######################################
+
+Solucionar los siguientes puntos mediante un script:
+1) Del archivo examen_final.csv genera un archivo csv para cada día, estos csv
+deben de guardarse en la carpeta de resultados
+2) De la información que se tiene, genera un corte de información para las columnas
+Lat y Long con los siguientes valores
+
+Lat > 21.0
+Lat < 24.0
+Long > -104.0
+Long < -100.0
+
+3) Calcula las medias de cada una de las variables Rain, Tmax, Tmin por día
+y guardalas en un csv en la carpeta de resultados
+
+4) Calcula las medias de cada una de las variables Rain, Tmax, Tmin por día
+del corte de información limitado por las columnas Lat y Long con los siguientes
+valores:
+
+Lat > 21.0
+Lat < 24.0
+Long > -104.0
+Long < -100.0
+
+y guardalas en un csv en la carpeta de resultados
+"""
+
+# librerias
+import Pandas as pd
+import numpy as np
+
+# función main()
+
+def main():
+
+
+
+
+if __name__ == '__main__':
+    main()
