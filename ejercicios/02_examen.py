26f2dfd18e9d06d365152b5b9bc74dc58d827cbb
JoseGpeSoto <joseguadalupesotolopez@gmail.com>
2018-03-07 12:56:29 -0600

    OK

---
+++ ejercicios/02_examen.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+#######################################
+# Author: Jorge Mauricio
+# Email: jorge.ernesto.mauricio@gmail.com
+# Date: 2018-02-01
+# Version: 1.0
+# No. Examen 2
+# Valor: 9 puntos
+#######################################
+
+Corrigue el siguiente script, recuerda realizar tus commits cada 5 min.
+"""
+import random
+
+for i in range(10):
+	a = random.randint(1,12)
+	b = random.randint(1,12)
+	question = "What is {} x {} ? ".format(a,b)
+	answer = int(input(question))
+	print(answer)
+	if answer == a*b:
+	  print ("Well done!")
+	else:
+	  print("No.")
