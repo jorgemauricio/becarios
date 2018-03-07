16198d39ee6a7de62c01e6b41239393e90814263
Miguel-INIF <Miguel_ING_Amador@outlook.com>
2018-03-07 09:59:03 -0600

    Correccion

--- ejercicios/01_examen.py
+++ ejercicios/01_examen.py
@@ -58,14 +58,14 @@ def main():
         dia = dia.mean()
         dia.to_csv('resultados/Medias(Dia{}).csv'.format(i))
     for i in range(1, 6):
-        print('Generando el csv de fltrado de informacion por Dia {}'.format(i))
-        dia = data[data['Dia'] == i]
-        dia = dia[(dia['Lat'] > 21.0 )& (dia['Lat'] < 24.0)]
-        dia = dia[(dia['Long'] > -100.0) & (dia['Long'] < -104.0)]
-        dia.to_csv('resultados/filtrado(Dia{}).csv'.format(i))
-        dia = dia.mean()
+        print('Generando el csv del Dia {} filtrado'.format(i))
+        dias = data[data['Dia'] == i]
+        dias = dias[(dias['Lat'] >= 21.0) & (dias['Lat'] <= 24.0)]
+        dias = dias[(dias['Long'] >= -104.0) & (dias['Long'] <= -100.0)]
+        dias.to_csv('resultados/filtrado(Dia{}).csv'.format(i))
+        dias = dias.mean()
         print('Generando el csv de media del fltrado de informacion por Dia {}'.format(i))
-        dia.to_csv('resultados/MediaFiltrado(Dia{}).csv'.format(i))
+        dias.to_csv('resultados/MediaFiltrado(Dia{}).csv'.format(i))
     print("*"*10)
     print('Generando csv calculando las Unidad de Calor')
     for i in range(0, len(maxt)):
