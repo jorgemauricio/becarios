efa07ac69507fd85f00c9314cf0570677d02c357
JoseGpeSoto <joseguadalupesotolopez@gmail.com>
2018-03-13 12:48:44 -0600

    OK

--- ejercicios/05_examen.py
+++ ejercicios/05_examen.py
@@ -15,7 +15,7 @@ de los 5 numéros más altos
 matrix = [[20,1,20,8,6],[19,0,5,8,10],[9,8,9,0,12],[11,2,13,4,5],[4,3,18,1,3]]
 
 resultado:
-Valor 20, posición 0,0
+Valor 20, C 0,0
 Valor 20, posición 0,2
 Valor 20, posición 3,2
 Valor 19, posición 1,0
@@ -28,5 +28,7 @@ for i in matrix:
 		arr.append(j)
 arr = sorted(arr)
 num_max = [arr[-5],arr[-4],arr[-3],arr[-2],arr[-1]]
-print(arr)
-for i in 
+for x in matrix:
+	for y in x:
+		if y in num_max:
+			print ('Valor {}'.format(y))
