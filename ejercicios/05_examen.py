3a0af17d915a8cfc36f4c40f3190059b7f457fdb
JoseGpeSoto <joseguadalupesotolopez@gmail.com>
2018-03-13 12:43:47 -0600

    OK

--- ejercicios/05_examen.py
+++ ejercicios/05_examen.py
@@ -21,3 +21,12 @@ Valor 20, posición 3,2
 Valor 19, posición 1,0
 Valor 18, posición 4,2
 """
+matrix = [[20,1,20,8,6],[19,0,5,8,10],[9,8,9,0,12],[11,2,13,4,5],[4,3,18,1,3]]
+arr =[]
+for i in matrix:
+	for j in i:
+		arr.append(j)
+arr = sorted(arr)
+num_max = [arr[-5],arr[-4],arr[-3],arr[-2],arr[-1]]
+print(arr)
+for i in 
