6f640cefa5cfe1eb4cfb5d46aa16fe7a2ac0c9e8
JoseGpeSoto <joseguadalupesotolopez@gmail.com>
2018-03-13 12:29:38 -0600

    OK

--- ejercicios/03_examen.py
+++ ejercicios/03_examen.py
@@ -23,12 +23,14 @@ Resultado:
 La palabra sOmetEmoS es palíndromo
 """
 
-frase1 = input("Inserte una palabra: ")
-frase1 = frase1.lower()
+frase = input("Inserte una palabra: ")
+frase1 = frase.lower()
 x = len(frase1)
-frase2 = []
+frase2 = ''
 while x > 0:
-	frase2.append(frase1[x-1])
+	frase2 = frase2 + frase1[x-1]
 	x = x-1
-frase2 = frase2.split(',')
-print(frase2)
+if frase1 == frase2:
+	print('La palabra {} es palíndromo'.format(frase))
+else:
+	print('La palabra {} no es palíndromo'.format(frase))
