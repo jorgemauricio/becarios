f3383b60ef0bd8f91332f39a8102d3749484c1fb
JoseGpeSoto <joseguadalupesotolopez@gmail.com>
2018-03-13 12:36:35 -0600

    OK

--- ejercicios/04_examen.py
+++ ejercicios/04_examen.py
@@ -19,6 +19,25 @@ Ingresa el día [1-31]: 23
 El 7mo día a partir de la fecha es [aaaa-mm-dd] 2016-8-30
 """
 
-ano = int(imput('Ingresa el año: '))
+ano = int(input('Ingresa el año: '))
 mes = int(input('Ingresa el mes: '))
 dia = int(input('Ingresa el año: '))
+
+if mes in (1, 3, 5, 7, 8, 10, 12): #Validacion de fecha
+    dias_mes = 31
+elif mes == 2:
+    if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0): #Si el mes se visiesto
+        dias_mes = 29
+    else:
+        dias_mes = 28
+elif mes in (4, 6, 9, 11):
+    dias_mes = 30
+
+dia = dia + 7
+while dia > dias_mes:
+	dia = dia - dias_mes
+	mes = mes + 1
+while mes > 12:
+	mes = mes - 12
+	ano = ano + 1
+print ('{:04d}-{:02d}-{:02d}'.format(ano,mes,dia))
