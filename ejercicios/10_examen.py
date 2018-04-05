ca922b09b08485dc3686b8a0028384387789d856
Jorge Mauricio <jorge.ernesto.mauricio@gmail.com>
2018-04-05 10:05:11 -0500

    examen 10

---
+++ ejercicios/10_examen.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+#######################################
+# Author: Jorge Mauricio
+# Email: jorge.ernesto.mauricio@gmail.com
+# Date: 2018-02-01
+# Version: 1.0
+# No. Examen 10
+# Valor: 10 puntos
+#######################################
+
+Generar un script capaz de resolver un cuadro mágico, mediante el uso de
+machine learning.
+
+El cuadro mágico es una tabla de grado primario donde se dispone de una
+serie de números enteros en un cuadrado o matriz de forma tal que la
+suma de los números por columnas, filas y diagonales principales sea la misma
+
+Para este ejercicio se va a resolver un cuadrado mágico de 3 x 3, la suma de
+sus valores horizontales, verticales y diagonales debe de ser 15.
+
+El valor principal que hay que tomar en cuenta, es el número de iteraciones
+que realiza la computadora para resolver este problema.
+
+__|__|__
+__|__|__
+  |  |
+
+"""
