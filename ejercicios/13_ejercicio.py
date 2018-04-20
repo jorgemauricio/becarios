f93fc455de27cc2427c046f0c04c8a702778a04a
carmen <medinamariadelcarmen75@gmail.com>
2018-04-20 09:48:06 -0500

    codigo_proy

--- ejercicios/13_ejercicio.py
+++ ejercicios/13_ejercicio.py
@@ -223,3 +223,15 @@ Palabra SUPERIOR se repite 1
 
 
 """
+
+entrada=raw_input("Introduce una cadena: ")
+ 
+diccionario={}
+ 
+for letra in entrada:
+    if diccionario.has_key(letra):
+        diccionario[letra]=diccionario[letra]+1
+    else:
+        diccionario[letra]=1
+ 
+print diccionario
